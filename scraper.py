18a580c552aa3617efea1b272401fb9226f5d4dd
Непорожнев Всеволод Васильевич <vvneporozhnev@edu.hse.ru>
2024-03-01 00:20:32 +0300

    1st commit

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import requests
 import datetime
+import pathlib
 import apimoex
 import pandas as pd
 
@@ -8,22 +9,27 @@ current_time = datetime.datetime.now().time()
 
 yesterday = datetime.date.today() - datetime.timedelta(days=1)
 
-with open("./TICK.txt", "r") as TICKs:
+with open("C:/Dor/courwo/TICK.txt", "r") as TICKs:
     TICKs = [line.rstrip() for line in TICKs]
 
-output_file = open("./tickers_data.txt".format(board), "w")
+output_file = open("C:/Dor/courwo/Database/{}/tickers_data.txt".format(board), "w")
 process = 0
 
 with requests.session() as session:
     for TICK in TICKs:
         process += 1
         print((process / len(TICKs)) * 100, ' %')
+        
+        # Получение котировок на определенную дату
         data = apimoex.get_board_history(session, TICK, board=board, start=yesterday, end=yesterday)
-        df = pd.DataFrame(data)
-        df = df[['TRADEDATE', 'CLOSE']]
-        for index, row in df.iterrows():
-            ticker_data = "{}, {}\n".format(TICK, row['CLOSE']) 
-            output_file.write(ticker_data)
-
+        
+        if data:
+            df = pd.DataFrame(data)
+            df = df[['TRADEDATE', 'CLOSE']]
+            for index, row in df.iterrows():
+                ticker_data = "{}, {}\n".format(TICK, row['CLOSE'])  # Изменили запись на тикер вместо даты
+                output_file.write(ticker_data)
+        else:
+            print(f"No data for {TICK}")
 
 output_file.close()
